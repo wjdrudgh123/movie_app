1d8be71aac937f1655edef5860cfe6a397d72a2e
Jung-Jaeho <wjdrudgh123@naver.com>
2020-09-30 15:47:58 +0900

    Updates

---
+++ precache-manifest.074d8aa8ace11def8f7da2fbe4bb5c97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6d656c9dbc53a3d2c7fc1b1d62e1ca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4f4dffcbf33cddc9604a",
+    "url": "/movie_app/static/css/main.f98409bb.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4dffcbf33cddc9604a",
+    "url": "/movie_app/static/js/main.c8c4f397.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
